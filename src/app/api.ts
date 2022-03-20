25a6ee050024274da5df37bbeef400bbedd03d58
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2022-03-20 23:31:09 +0530

    api updated

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://predict2105.herokuapp.com/",
   headers: {
     "Content-Type": "multipart/form-data",
   },
